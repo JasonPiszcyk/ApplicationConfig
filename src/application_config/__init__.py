5d8a953fee7640a1e29c1bd473a0b0b705c868a9
JasonPiszcyk <64060251+JasonPiszcyk@users.noreply.github.com>
2023-11-19 07:06:45 +1030

    Modified to import correctly

---
+++ src/application_config/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+* __init__.py
+*
+* Copyright (c) 2023 Iocane Pty Ltd
+*
+* @author: Jason Piszcyk
+*
+* Module initialisation
+*
+'''
+from .application_config import ApplicationConfig as ApplicationConfig
+from .application_config import Config as Config
